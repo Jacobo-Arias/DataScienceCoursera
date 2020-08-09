894207348eaca5420295c23133db2f61a278423d
Jacobo Arias <jacobo.a12@utp.edu.co>
2020-08-09 18:33:25 -0500

    R proggraming finished

--- R_programming/Semana2.R
+++ R_programming/Semana2.R
@@ -2,7 +2,7 @@
 # Week 2
 # R Programming
 # Data science by Johns Hopkings University
-# Coursera
+# Coursera 
 
 
 download_path <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
